b632e766f66c366c932cd5883b8a14d4ed812f9f
xlim9 <xinyu.lim@meetkai.com>
2023-05-15 21:36:14 +0800

    Add SQLite database

--- nostr/database.py
+++ nostr/database.py
@@ -1,7 +1,10 @@
 import logging
+import sqlite3
 from typing import Collection, Dict
-from .model import Event
 
+import aiosqlite
+
+from .model import Event
 
 logging.basicConfig(
     format="%(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
@@ -19,10 +22,34 @@ class Database:
 
 class InMemoryDatabase:
     def __init__(self):
-        self.db: Dict[str, Event] = {}
+        self._database: Dict[str, Event] = {}
 
     async def add(self, event: Event) -> None:
-        self.db[event.id] = event
+        self._database[event.id] = event
 
     async def query(self) -> Collection[Event]:
-        return list(self.db.values())
+        return list(self._database.values())
+
+
+class SQLiteDatabase:
+    def __init__(self):
+        database = sqlite3.connect("test.db")
+        database.execute("CREATE TABLE IF NOT EXISTS events (id TEXT, message TEXT)")
+        database.close()
+
+    async def add(self, event: Event) -> None:
+        id = event.id
+        message = event.to_client_message()
+        query = f"INSERT INTO events(id, message) VALUES ('{id}', '{message}')"
+        db = await aiosqlite.connect("test.db")
+        await db.execute(query)
+        await db.commit()
+        await db.close()
+
+    async def query(self) -> Collection[Event]:
+        db = await aiosqlite.connect("test.db")
+        results = []
+        async with db.execute("SELECT message FROM events") as cursor:
+            async for row in cursor:
+                results.append(Event.from_client_message(row[0]))
+        return results
